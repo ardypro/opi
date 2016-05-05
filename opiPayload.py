2624bbbae5848e4c4d732e0b7d3cd169802ec3a8
Jack Zhong <jzopen@yeah.net>
2016-05-05 21:20:44 +0800

    monitor orangepi memory and cpu usage in python

---
+++ opiPayload.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  opi.py
+#  
+#  Copyright 2016 Jack Zhong <ardypro@vostro>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  This project is to monitor orange pi's cpu and memory usage
+#
+
+import psutil
+import time
+import requests
+import json
+
+def initial():
+	print '开始监控Orange Pi系统资源'
+	return 0
+	
+def main(args):
+	initial()
+	
+	api='http://api.heclouds.com/devices/1100454/datapoints?type=3'
+	api_key='w2ZpZfGcjeZqRaijTM8YK9tnMeA='
+
+	while (True):
+		cpuload = psutil.cpu_percent(interval=None)
+		memload = psutil.virtual_memory()[2]
+		
+		payload={'memload':memload, 'cpuload':cpuload, 'connstatus':1}
+		_data=json.dumps(payload)
+		_headers={'api-key':api_key}
+		r=requests.post(api, _data, headers=_headers)
+
+		#print 'cpu load ', cpuload
+		#print 'mem load ', memload
+		
+		time.sleep(1)
+	return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
